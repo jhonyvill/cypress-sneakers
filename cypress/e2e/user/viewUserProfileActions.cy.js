36d1703a0a4578f92d6fa1e551e1493020988d59
Jhony Vill <jhonyvill.dev@gmail.com>
2025-04-22 15:08:29 -0300

    add user profile tests

---
+++ cypress/e2e/user/viewUserProfileActions.cy.js
@@ -0,0 +1,27 @@
+describe("View user profile actions.", () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.config("baseUrl")}`);
+  });
+
+  it("should display links to log out and view user profile information.", () => {
+    cy.log("Esse teste irá falhar. Há um bug proposital aqui.");
+
+    cy.get('img[alt="imagem de perfil"]').click();
+    cy.get('[data-testid="profile-modal"]').should("be.visible");
+    cy.get('[data-testid="profile-modal"] a[href="/user"]').should(
+      "have.text",
+      "Ver informações de perfil"
+    );
+    cy.get('[data-testid="profile-modal"] a[href="/logout"]').should(
+      "have.text",
+      "Sair"
+    );
+  });
+
+  it("should close the user info modal when clicking the profile picture thumbnail.", () => {
+    cy.get('img[alt="imagem de perfil"]').click();
+    cy.get('[data-testid="profile-modal"]').should("be.visible");
+    cy.get('img[alt="imagem de perfil"]').click();
+    cy.get('[data-testid="profile-modal"]').should("not.exist");
+  });
+});
